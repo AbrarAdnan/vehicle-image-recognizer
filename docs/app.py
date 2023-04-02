3fbed1678b67cd62599d1e6e62876a798fea0c59
Abrar Adnan <abrar.adnan01@gmail.com>
2023-04-02 16:44:05 +0600

    small fixes

--- docs/app.py
+++ docs/app.py
@@ -4,27 +4,7 @@ app = Flask(__name__,static_url_path='/static')
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    if request.method == 'POST':
-        input_image = request.files['image']
-        print("python image")
-        output = predict_genres(input_image)
-        print(output)
-        return render_template('index.html')
-    else:
-        return render_template('index.html') 
-
-def predict_genres(input_image):
-    response = requests.post("https://abrar-adnan-vehicle-recognizer.hf.space/run/predict", json={
-    "data": [
-    {
-    "data": input_image
-    }
-    ]
-    }).json()
-
-    data = response["data"]
-    return data
-
+    return render_template('index.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
